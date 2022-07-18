a5e33b426555714654b9919e6707e5b0d33505cc
Jiikuan <109325319+Jiikuan@users.noreply.github.com>
2022-07-18 20:19:04 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,18 +1,29 @@
+import { tiktokdl } from '@bochilteam/scraper'
 
-const { tiktokdl } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
     const { author: { nickname }, video, description } = await tiktokdl(args[0])
     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
-*Nickname:* ${nickname}
-*Description:* ${description}
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `*「 T I K T O K 」*
+     ████████▀▀▀████
+     ████████────▀██
+     ████████──█▄──█
+     ███▀▀▀██──█████
+     █▀──▄▄██──█████
+     █──█████──█████
+     █▄──▀▀▀──▄█████
+     ███▄▄▄▄▄███████
+─────────────*
+*📛Nickname:* ${nickname}
+*📒Description:* ${description}
+      𝕵𝖎𝖎𝖐𝖚𝖆𝖓 | 𝙕𝙮𝙠𝙪𝙖𝙣
 `.trim(), m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 
-handler.command = /^(tik(tok)?(dl)?)$/i
+export default handler
 
-module.exports = handler
+//BY FANGZ
